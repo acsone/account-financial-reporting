366d6e5d27fbbf00a6a272609e0d4662c9cd237f
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2016-01-27 08:21:30 +0100

    [IMP][mis_builder] MIS Builder widget improvement, add print and export button in the widget.

--- mis_builder/report/mis_builder_xls.py
+++ mis_builder/report/mis_builder_xls.py
@@ -81,7 +81,7 @@ class mis_builder_xls(report_xls):
 
         # get the computed result of the report
         data = self.pool.get('mis.report.instance').compute(
-            self.cr, self.uid, objects[0].id)
+            self.cr, self.uid, objects[0].id, self.context)
 
         # Column headers
         header_name_list = ['']
