f472cc44d704c05a4e42f7169a27845c7a7c29b8
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2023-05-01 18:40:58 +0200

    Show analytic account code in general ledger

--- account_financial_report_qweb/report/general_ledger.py
+++ account_financial_report_qweb/report/general_ledger.py
@@ -1144,7 +1144,7 @@ SELECT
             """
         query_inject_move_line += """
     CONCAT_WS(' - ', NULLIF(ml.ref, ''), NULLIF(ml.name, '')) AS label,
-    aa.name AS cost_center,
+    CONCAT_WS(' - ', aa.code, aa.name) AS cost_center,
     ml.debit,
     ml.credit,
         """
